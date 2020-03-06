35c3c5ee13c86d49afc425f4f2d3536615700c23
sato <satoyu130@gmail.com>
2020-03-06 02:32:25 +0000

    static_pageテスト作成

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'bootstrap', '~>4.1.1'
 group :development, :test do
   gem 'byebug', '9.0.6', platform: :mri
   gem 'rspec-rails', '~>3.8.0'
+  gem 'factory_bot_rails', '~> 4.10.0'
 end
 
 group :development do
--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -7,9 +7,15 @@ RSpec.describe 'StaticPages', type: :system do
         visit root_path
       end
       
-      it "なんでも掲示板の文字列が存在する事を確認" do
+      it "なんでも掲示板の文字列が存在することを確認" do
         expect(page).to have_content "なんでも掲示板"
       end
+      
+      it "正しいタイトルが表示されることを確認"
+      context "スレッド関係"
+        it "ページネーションが機能する事"
+        it "検索が機能すること(タイトル検索)"
+        it "検索が機能すること(本文検索)"
     end
   end
 end
