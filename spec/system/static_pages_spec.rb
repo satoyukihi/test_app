74a9a1096806b5d9fff8a8df7799badff2f6ad14
sato <satoyu130@gmail.com>
2020-03-06 03:06:09 +0000

    動的タイトルの導入

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -7,11 +7,14 @@ RSpec.describe 'StaticPages', type: :system do
         visit root_path
       end
       
-      it "なんでも掲示板の文字列が存在することを確認" do
+      it "なんでも掲示板の文字列が存在すること" do
         expect(page).to have_content "なんでも掲示板"
       end
       
-      it "正しいタイトルが表示されることを確認"
+      it "正しいページタイトルが表示されること" do
+        expect(page).to have_title "なんでも掲示板-top_page"
+      end
+      
       context "スレッド関係"
         it "ページネーションが機能する事"
         it "検索が機能すること(タイトル検索)"
